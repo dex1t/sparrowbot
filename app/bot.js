8f5dd46aff8dfb516e573978f08c35842d6a78be
dex1t <dex1t@degoo.org>
2017-04-30 22:18:13 +0900

    Refactoring

--- app/bot.js
+++ app/bot.js
@@ -2,49 +2,56 @@ const Botkit = require('botkit');
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
-const redis = require('botkit-storage-redis');
+
+const redisURL = url.parse(process.env.REDIS_URL);
+const redisStorage = require('botkit-storage-redis')({
+  namespace: 'sparrowbot',
+  host: redisURL.hostname,
+  port: redisURL.port,
+  auth_pass: redisURL.auth && redisURL.auth.split(':')[1],
+});
 
 class Bot {
-  static run() {
-    const redisURL = url.parse(process.env.REDIS_URL);
-    const redisStorage = redis({
-      namespace: 'sparrowbot-dev',
-      host: redisURL.hostname,
-      port: redisURL.port,
-      auth_pass: redisURL.auth && redisURL.auth.split(':')[1],
-    });
-
-    const controller = Botkit.slackbot({
+  constructor() {
+    this.controller = Botkit.slackbot({
       debug: false,
       clientId: process.env.SLACK_APP_CLIENT_ID,
       clientSecret: process.env.SLACK_APP_CLIENT_SECRET,
       scopes: ['bot'],
       storage: redisStorage,
     });
+  }
 
-    controller.spawn({
-      token: process.env.SLACK_BOT_TOKEN,
-    }).startRTM((err) => {
-      if (err) {
-        throw new Error(err);
-      }
-    });
-
-    controller.setupWebserver(process.env.PORT, () => {
-      controller.createOauthEndpoints(controller.webserver, (err, req, res) => {
+  setupWebserver() {
+    this.controller.setupWebserver(process.env.PORT, () => {
+      this.controller.createOauthEndpoints(this.controller.webserver, (err, req, res) => {
         if (err) {
           res.status(500).send(`ERROR: ${err}`);
         } else {
           res.send('Success!');
         }
       });
-      controller.createWebhookEndpoints(controller.webserver);
+      this.controller.createWebhookEndpoints(this.controller.webserver);
     });
+  }
 
+  setupSkills() {
     fs.readdirSync(path.join(__dirname, 'skills')).forEach((file) => {
       const Skill = require(`./skills/${file}`); // eslint-disable-line
-      new Skill(controller).run();
+      new Skill(this.controller).run();
     });
+  }
+
+  run() {
+    this.controller.spawn({
+      token: process.env.SLACK_BOT_TOKEN,
+    }).startRTM((err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+    this.setupWebserver();
+    this.setupSkills();
   }
 }
 
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (process.env.SLACK_BOT_TOKEN &&
     process.env.TWITTER_CONSUMER_SECRET &&
     process.env.TWITTER_ACCESS_TOKEN &&
     process.env.TWITTER_ACCESS_SECRET) {
-  Bot.run();
+  new Bot().run();
 } else {
   console.log('Error: Specify Slack Token and Twitter Token in environment');
   process.exit(1);
