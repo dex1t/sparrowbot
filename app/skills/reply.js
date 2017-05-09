ffb2262af38fc86197e2273d7ffd21663d08d104
dex1t <dex1t@degoo.org>
2017-05-10 04:39:00 +0900

    Attach confirmation tweet

--- app/skills/reply.js
+++ app/skills/reply.js
@@ -5,6 +5,13 @@ class Reply {
   }
 
   run() {
+    this.client.get('account/verify_credentials', (err, res) => {
+      if (err) {
+        this.controller.botkit.log('Error: account/verify_credentials', err);
+      }
+      this.myTwitter = res;
+    });
+
     this.controller.on('interactive_message_callback', (bot, msg) => {
       if (msg.actions[0].name !== 'reply') return;
 
@@ -12,12 +19,27 @@ class Reply {
       parentMsg.channel = msg.channel;
       parentMsg.user = msg.user; // ask user who pressed reply button
 
+      console.log(this.myTwitter);
       const confirmReply = (res, convo) => {
-        convo.ask('これでツイートしてOK?', [
+        const inReplyTo = msg.callback_id;
+        const replyText = `@${msg.actions[0].value} ${res.text}`;
+
+        const askMessage = {
+          text: 'Would you like to post this reply? :rocket: *(yes/no)*',
+          attachments: [{
+            author_name: `@${this.myTwitter.screen_name}`,
+            author_icon: this.myTwitter.profile_image_url_https,
+            author_link: `https://twitter.com/${this.myTwitter.screen_name}`,
+            text: replyText,
+            fallback: replyText,
+            color: this.myTwitter.profile_link_color,
+          }],
+        };
+        convo.ask(askMessage, [
           {
             pattern: bot.utterances.yes,
             callback: (r, c) => {
-              c.say(`Cool. ${msg.callback_id} ${msg.actions[0].value} ${res.text}`);
+              c.say('tweeted :ok_hand:');
               c.next();
             },
           },
@@ -32,7 +54,7 @@ class Reply {
       };
 
       const askReplyContent = (res, convo) => {
-        convo.ask(`<@${parentMsg.user}> How do you reply?`, [
+        convo.ask(`<@${parentMsg.user}> Please write your reply here :writing_hand:`, [
           {
             default: true,
             callback: (r, c) => {
